bb23cae7c20679d2bffb4238ef00a84698b2a2f8
0x1do <ido.schwartzer@gmail.com>
2025-11-12 11:15:25 +0200

    created function encoder signature

--- src/kernel.h
+++ src/kernel.h
@@ -5,19 +5,28 @@
 typedef enum { false = 0, true = 1 } bool;
 
 void kernel_main(void);
+struct DescriptorEntry encodeDescriptor(unsigned int SegLimit,
+										int *BaseAddress,
+										unsigned int Type,
+										bool S,
+										unsigned int DPL,
+										bool P,
+										bool AVL,
+										bool L,
+										bool D_B,
+										bool G);
 
 struct DescriptorEntry {
-    unsigned int SegLimit15_00 : 16;  // Segment Limit 15-00
-    unsigned int Base23_00 : 24;      // Base Address 23-00 
-    unsigned int Type : 4;            // Type
-    unsigned int S : 1;               // S
-    unsigned int DPL : 2;             // DPL
-    unsigned int P : 1;               // P
-    unsigned int SegLimit19_16 : 4;   // Seg. Limit 19-16
-    unsigned int AVL : 1;             // AVL
-    unsigned int L : 1;               // L
-    unsigned int D_B : 1;             // D/B
-    unsigned int G : 1;               // G
-    unsigned int Base31_24 : 8;       // Base 31-24
+	unsigned int SegLimit15_00 : 16; // Segment Limit 15-00
+	unsigned int Base23_00 : 24;	 // Base Address 23-00
+	unsigned int Type : 4;			 // Type
+	unsigned int S : 1;				 // S
+	unsigned int DPL : 2;			 // DPL
+	unsigned int P : 1;				 // P
+	unsigned int SegLimit19_16 : 4;	 // Seg. Limit 19-16
+	unsigned int AVL : 1;			 // AVL
+	unsigned int L : 1;				 // L
+	unsigned int D_B : 1;			 // D/B
+	unsigned int G : 1;				 // G
+	unsigned int Base31_24 : 8;		 // Base 31-24
 };
-
